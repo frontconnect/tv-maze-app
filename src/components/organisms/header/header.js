066f9d6c64f85df18a54abcaf0d744cf724decb5
Yaprak Ayazoglu <ya@192.168.1.101>
2018-12-14 09:46:30 +0100

    Define header component

---
+++ src/components/organisms/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logo from '../../molecules/logo/';
+
+import './header.css';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <Logo/>
+    </header>
+  );
+}
